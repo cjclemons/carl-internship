794556f6b51bd39b1ee9a8794a7d8accc1732139
cjclemons <carlj.clemons487@gmail.com>
2025-09-26 18:43:57 -0400

    css not working

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -10,7 +10,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
   const [hotCollections, setHotCollections] = useState([]);
-  
 
   async function fetchHotCollections() {
     const { data } = await axios.get(
@@ -28,7 +27,7 @@ const HotCollections = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
   };
   return (
@@ -41,42 +40,53 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <HotCollections {...settings}>
-            {hotCollections.map((collect) => (
-              <div
-                className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                key={collect.id}
-              >
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to={`/item-details/${collect.nftId}`}>
-                      <img
-                        src={collect.nftImage}
-                        className="lazy img-fluid"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft_coll_pp">
-                    <Link to={`/author/${collect.authorId}`}>
-                      <img
-                        className="lazy pp-coll"
-                        src={collect.authorImage}
-                        alt=""
-                      />
-                    </Link>
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{collect.title}</h4>
-                    </Link>
-                    <span>ERC-{collect.code}</span>
+          <div className="owl-carousel owl-theme owl-loaded owl-drag">
+            <Slider {...settings}>
+              {hotCollections.map((collect) => (
+                <div
+                  className="slick-slide slick-cloned slick-active"
+                  key={collect.id}
+                >
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to={`/item-details/${collect.nftId}`}>
+                        <img
+                          src={collect.nftImage}
+                          className="lazy img-fluid"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to={`/author/${collect.authorId}`}>
+                        <img
+                          className="lazy pp-coll"
+                          src={collect.authorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{collect.title}</h4>
+                      </Link>
+                      <span>ERC-{collect.code}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </HotCollections>
+              ))}
+            </Slider>
+            <div className="owl-nav">
+              <button role="presentation" className="owl-prev">
+                <span aria-label="Previous">-</span>
+              </button>
+              <button role="presentation" className="owl-next">
+                <span aria-label="Next">+</span>
+              </button>
+            </div>
+            <div className="owl-dots"></div>
+          </div>
         </div>
       </div>
     </section>
