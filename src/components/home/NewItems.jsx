510169dbf7ae2256e5c86bcfb9ceccce44eab0db
cjclemons <carlj.clemons487@gmail.com>
2025-09-27 10:40:07 -0400

    new items nothing happened

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const NewItems = () => {
+
+  const [newItems, setNewItems] = useState([])
+  async function fetchNewItems() {
+    const {data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
+
+  }
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
