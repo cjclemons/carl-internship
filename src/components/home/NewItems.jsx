a5ca8b7e2bfdf99051d0753ba0345345acf4bb8c
cjclemons <carlj.clemons487@gmail.com>
2025-09-27 13:14:20 -0400

    added a div

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -15,7 +15,7 @@ const NewItems = () => {
     );
     setNewItems(data);
   }
-// comme
+
   useEffect(() => {
     fetchNewItems();
   }, []);
@@ -86,125 +86,131 @@ const NewItems = () => {
             </div>
           </div>
           //{" "}
-          {newItems.length ? (
-            <Slider className="owl-theme" {...settings}>
-              {newItems.map((item) => (
-                <div className="nft__item" key={item.id}>
-                  <div className="author_list_pp">
-                    <Link
-                      to={`/author${item.authorId}`}
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="Creator: Monica Lucas"
-                    >
-                      <img className="lazy" src={item.authorImage} alt="" />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  {item.expiryDate && (
-                    <CountDown expiryDate={item.expiryDate} />
-                  )}
+          <div className="owl-carousel owl-theme owl-loaded owl-drag">
+            {newItems.length ? (
+              <Slider className="owl-theme" {...settings}>
+                {newItems.map((item) => (
+                  <div className="nft__item" key={item.id}>
+                    <div className="author_list_pp">
+                      <Link
+                        to={`/author${item.authorId}`}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <img className="lazy" src={item.authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    {item.expiryDate && (
+                      <CountDown expiryDate={item.expiryDate} />
+                    )}
 
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
                         </div>
                       </div>
+
+                      <Link to={`/item-details/${item.nftId}`}>
+                        <img
+                          src={item.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={`/item-details/${item.nftId}`}>
+                        <h4>{item.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{item.price} ETH</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{item.likes}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            ) : (
+              <Slider className="owl-theme" {...settings}>
+                {new Array(8).fill(0).map((_, index) => (
+                  <div className="nft__item" key={index}>
+                    <div className="author_list_pp">
+                      <Link
+                        to={``}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <Skeleton
+                          width="50px"
+                          height="50px"
+                          borderRadius="50%"
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
                     </div>
 
-                    <Link to={`/item-details/${item.nftId}`}>
-                      <img
-                        src={item.nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to={`/item-details/${item.nftId}`}>
-                      <h4>{item.title}</h4>
-                    </Link>
-                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a
+                              href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a
+                              href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Link to={``}>
+                        <Skeleton width="100%" height="350px" />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={``}>
+                        <Skeleton width="180px" height="30px" />
+                      </Link>
+                      <Skeleton width="100px" height="20px" />
+                    </div>
                     <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{item.likes}</span>
+                      <Skeleton width="30px" height="15px" />
                     </div>
                   </div>
-                </div>
-              ))}
-            </Slider>
-          ) : (
-            <Slider className="owl-theme" {...settings}>
-              {new Array(8).fill(0).map((_, index) => (
-                <div className="nft__item" key={index}>
-                  <div className="author_list_pp">
-                    <Link
-                      to={``}
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="Creator: Monica Lucas"
-                    >
-                      <Skeleton width="50px" height="50px" borderRadius="50%" />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a
-                            href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a
-                            href="https://twitter.com/intent/tweet?url=https://gigaland.io"
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-
-                    <Link to={``}>
-                      <Skeleton width="100%" height="350px" />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to={``}>
-                      <Skeleton width="180px" height="30px" />
-                    </Link>
-                    <Skeleton width="100px" height="20px" />
-                  </div>
-                  <div className="nft__item_like">
-                    <Skeleton width="30px" height="15px" />
-                  </div>
-                </div>
-              ))}
-            </Slider>
-          )}
+                ))}
+              </Slider>
+            )}
+          </div>
         </div>
       </div>
     </section>
