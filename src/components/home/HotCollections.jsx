14e8fc7b0447b5579fc159b5ba8cab2921a4ce78
cjclemons <carlj.clemons487@gmail.com>
2025-09-27 10:42:23 -0400

    Hot collections put in my main

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import axios from "axios";
-
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
